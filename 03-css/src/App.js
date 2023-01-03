9d05e4c54ba40be4da48ca253c32ba51e73bd1b6
lauromiottojr <junior.miotto36@gmail.com>
2023-01-03 16:32:06 -0300

    CSS

---
+++ 03-css/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './components/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React com css</h1>
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
