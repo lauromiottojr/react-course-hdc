55aa682de329363d8cace8c7ccd191f9e8ea80f2
lauromiottojr <junior.miotto36@gmail.com>
2023-01-02 22:30:40 -0300

    Avancando em React

---
+++ 02-avancando/src/App.js
@@ -0,0 +1,15 @@
+import City from './assets/city.jpg'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      <img src={City} alt="Cidade" />
+      <img src="/img/img1.jpg" alt="Paisagem" />
+    </div>
+  );
+}
+
+export default App;
