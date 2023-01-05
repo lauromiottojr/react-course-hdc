ee8eceae3e7c5f9b138ae12c374ac24a93978c13
lauromiottojr <junior.miotto36@gmail.com>
2023-01-05 15:06:07 -0300

    Finish css module

--- 03-css/src/App.js
+++ 03-css/src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import MyComponent from './components/MyComponent';
+import Title from './components/Title';
 
 function App() {
+
+  const n = 15;
+
+  const redTitle = false;
+
   return (
     <div className="App">
       <h1>React com css</h1>
       <MyComponent />
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
+        Este elemento foi estilizado de forma inline
+      </p>
+
+      <p style={n > 10 ? { backgroundColor: "purple" } : { backgroundColor: "orange" }}>
+        CSS dinamico
+      </p>
+      <h2 className={redTitle ? "redTitle" : "title"}>
+        Esse título terá classe dinamica
+      </h2>
+      <Title />
     </div>
   );
 }
