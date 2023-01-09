b6a06f4476da9b9438a0dee76e5905f9927da5d4
lauromiottojr <junior.miotto36@gmail.com>
2023-01-09 16:06:12 -0300

    Config guessedLetters and wrongLetters from app.js

--- 05-secret-word/src/App.js
+++ 05-secret-word/src/App.js
@@ -56,8 +56,24 @@ function App() {
   }
 
   const verifyLetter = (letter) => {
-    console.log(letter)
-
+    const normalizedLetter = letter.toLowerCase()
+    if (guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)) {
+      return;
+    }
+    if (letters.includes(normalizedLetter)) {
+      // Get currently guessedletters and add normalizedletters if user guessed it
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter
+      ])
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter
+      ])
+    }
+    console.log(guessedLetters)
+    console.log(wrongLetters)
   }
 
   const retry = () => {
