f3d61e0f10868e78990f7e6ab184f9c4838d819e
lauromiottojr <junior.miotto36@gmail.com>
2023-01-10 10:20:04 -0300

    Getting data from API and listing it

--- 06-http-react/src/App.js
+++ 06-http-react/src/App.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react'
+
 import './App.css';
 
+const url = 'http://localhost:3000/products';
+
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  // getting datas from api
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json(); // transform res to JSON 
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product)=>(
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
