5b282a7a776d1433c82e3851d4453c330db066c7
lauromiottojr <junior.miotto36@gmail.com>
2023-01-11 09:33:53 -0300

    Config Navbar component

--- 07-react-router/src/App.js
+++ 07-react-router/src/App.js
@@ -1,11 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Navbar from './components/Navbar'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Router</h1>
+      <BrowserRouter>
+        <Navbar /> {/* navbar must be in BrowserRouter cause there are react-router elements on it*/}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
