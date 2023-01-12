3154b9c73ea6eac3b64354a740fa5350adc7bb87
lauromiottojr <junior.miotto36@gmail.com>
2023-01-11 21:14:31 -0300

    Refatoring context

--- 08-context/src/pages/Home.js
+++ 08-context/src/pages/Home.js
@@ -1,12 +1,16 @@
-import { useContext } from "react"
+//import { useContext } from "react"
 
-import { CounterContext } from "../context/CounterContext"
+//import { CounterContext } from "../context/CounterContext"
 
 import ChangeCounter from "../components/ChangeCounter"
 
+import { useCounterContext } from "../hooks/useCounterContext"
+
 const Home = () => {
 
-  const { counter } = useContext(CounterContext)
+  //const { counter } = useContext(CounterContext)
+
+  const { counter } = useCounterContext()
 
   return (
     <div>
