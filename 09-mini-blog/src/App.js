e2097ff86d077319564b7b9c5750c1ef5190e9eb
lauromiottojr <junior.miotto36@gmail.com>
2023-01-16 09:08:06 -0300

    Config react router dom

--- 09-mini-blog/src/App.js
+++ 09-mini-blog/src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+
+import About from './pages/About/About'
+import Home from './pages/Home/Home'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Mini Blog</h1>
+      <BrowserRouter>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
