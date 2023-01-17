b23260972a3dbaf21337e1de609d5048586a00bd
lauromiottojr <junior.miotto36@gmail.com>
2023-01-17 10:45:51 -0300

    Creating Dashboard and CreatePost pages

--- 09-mini-blog/src/App.js
+++ 09-mini-blog/src/App.js
@@ -15,6 +15,8 @@ import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import CreatePost from './pages/CreatePost/CreatePost'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 import './App.css'
 
@@ -37,7 +39,7 @@ function App() {
 
   return (
     <div className="App">
-      <AuthProvider value={user}>
+      <AuthProvider value={{ user }}>
         <BrowserRouter>
           <Navbar />
           <div className='container'>
@@ -46,6 +48,8 @@ function App() {
               <Route path='/about' element={<About />} />
               <Route path='/login' element={<Login />} />
               <Route path='/register' element={<Register />} />
+              <Route path='/posts/create' element={<CreatePost />} />
+              <Route path='/dashboard' element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
