3ce9c70a5d58ad00f0064cee0190d3d6991e4592
lauromiottojr <junior.miotto36@gmail.com>
2023-01-17 10:22:44 -0300

    Sending user to context

--- 09-mini-blog/src/App.js
+++ 09-mini-blog/src/App.js
@@ -1,7 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth'
 
+//hooks
+import { useState, useEffect } from 'react'
+import { useAuthentication } from './hooks/useAuthentication'
+
+//context
 import { AuthProvider } from './context/AuthContext'
 
+//pages
 import About from './pages/About/About'
 import Home from './pages/Home/Home'
 import Footer from './components/Footer/Footer'
@@ -12,9 +19,25 @@ import Register from './pages/Register/Register'
 import './App.css'
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <AuthProvider>
+      <AuthProvider value={user}>
         <BrowserRouter>
           <Navbar />
           <div className='container'>
