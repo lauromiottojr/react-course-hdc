aefe36d24abb7a7a15e6116a3e2a325f2effcd55
lauromiottojr <junior.miotto36@gmail.com>
2023-01-18 20:23:04 -0300

    Building Dashboard

--- 09-mini-blog/src/pages/Dashboard/Dashboard.js
+++ 09-mini-blog/src/pages/Dashboard/Dashboard.js
@@ -1,8 +1,30 @@
+import { Link } from 'react-router-dom'
+
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
+
 import styles from './Dashboard.module.css'
 
 const Dashboard = () => {
+
+    const { user } = useAuthValue()
+    const uid = user.uid
+
+    const posts = []
+
     return (
-        <div>Dashboard</div>
+        <div>
+            <h2>Dashboard</h2>
+            <p>Gerencie seus posts</p>
+            {posts && posts.length === 0 ? (
+                <div className={styles.noPosts}>
+                    <p>Não foi encontrado nenhum post</p>
+                    <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
+                </div>
+            ) : (
+                <div>Tem post</div>
+            )}
+        </div>
     )
 }
 
