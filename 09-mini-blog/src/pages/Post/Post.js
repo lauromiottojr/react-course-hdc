348a22fa3ce1ee4d738dd5afb7c30761264293d3
lauromiottojr <junior.miotto36@gmail.com>
2023-01-18 20:15:48 -0300

    Config Post CSS

--- 09-mini-blog/src/pages/Post/Post.js
+++ 09-mini-blog/src/pages/Post/Post.js
@@ -9,11 +9,19 @@ const Post = () => {
     const { document: post, loading } = useFetchDocument("posts", id)
 
     return (
-        <div>
+        <div className={styles.postContainer}>
             {loading && <p>Carregando post...</p>}
             {post && (
                 <>
                     <h1>{post.title}</h1>
+                    <img src={post.image} alt={post.title} />
+                    <p>{post.body}</p>
+                    <h3>Este post trata-se sobre:</h3>
+                    {post.tagsArray.map((tag) => (
+                        <div className={styles.tags}>
+                            <p key={tag}><span>#{tag}</span></p>
+                        </div>
+                    ))}
                 </>
             )}
         </div>
