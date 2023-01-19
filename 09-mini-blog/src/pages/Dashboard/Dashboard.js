39e9bee54d97a108300e47e45584effb47b30ed7
lauromiottojr <junior.miotto36@gmail.com>
2023-01-19 08:56:15 -0300

    Config Dashboard page

--- 09-mini-blog/src/pages/Dashboard/Dashboard.js
+++ 09-mini-blog/src/pages/Dashboard/Dashboard.js
@@ -12,6 +12,14 @@ const Dashboard = () => {
 
     const { documents: posts, loading } = useFetchDocuments("posts", null, uid)
 
+    const deleteDocument = (id) => {
+
+    }
+
+    if (loading) {
+        return (<p>Carregando...</p>)
+    }
+
     return (
         <div>
             <h2>Dashboard</h2>
@@ -22,11 +30,26 @@ const Dashboard = () => {
                     <Link to="/posts/create" className='btn'>Criar primeiro post</Link>
                 </div>
             ) : (
-                <div>{}</div>
+                <>
+                    <div>
+                        <span>Título</span>
+                        <span>Ações</span>
+                    </div>
+                    {posts && posts.map((post) => (
+                        <div key={post.id}>
+                            <p>{post.title}</p>
+                            <div>
+                                <Link to={`/posts/${post.id}`}
+                                    className='btn btnOutline'>Ver</Link>
+                                <Link to={`/posts/edit/${post.id}`}
+                                    className='btn btnOutline'>Editar</Link>
+                                <button onClick={() => deleteDocument(post.id)}
+                                    className='btn btnOutline btnDanger'>Excluir</button>
+                            </div>
+                        </div>
+                    ))}
+                </>
             )}
-            {posts && posts.map((post)=> (
-                <h3>{post.title}</h3>
-            ))}
         </div>
     )
 }
