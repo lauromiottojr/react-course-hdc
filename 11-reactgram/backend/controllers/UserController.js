c85211ff477e62d3bf0819fab15633e07833801c
lauromiottojr <junior.miotto36@gmail.com>
2023-01-23 15:11:02 -0300

    Create new user

--- 11-reactgram/backend/controllers/UserController.js
+++ 11-reactgram/backend/controllers/UserController.js
@@ -14,7 +14,36 @@ const generateToken = (id) => {
 
 // register user and signin
 const register = async (req, res) => {
-    res.send("Registro")
+    const { name, email, password } = req.body
+
+    //check if user exists
+    const user = await User.findOne({ email })
+    if (user) {
+        res.status(422).json({ errors: ["E-mail já cadastrado!"] })
+        return
+    }
+
+    // generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // if user was created, return token
+    if (!newUser) {
+        res.status(422).json({ errors: ["Houve um erro, por favor tente novamente mais tarde."] })
+        return
+    }
+    res.status(201).json({
+        _id: newUser.id,
+        token: generateToken(newUser._id)
+    })
+
 }
 
 module.exports = {
