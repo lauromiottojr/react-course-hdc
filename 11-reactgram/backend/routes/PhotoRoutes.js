48f1366ba0949ef020d7c32d95299d2cc262ebb3
lauromiottojr <junior.miotto36@gmail.com>
2023-01-25 13:23:18 -0300

    get all photos

--- 11-reactgram/backend/routes/PhotoRoutes.js
+++ 11-reactgram/backend/routes/PhotoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 // controller
-const { insertPhoto, deletePhoto } = require("../controllers/PhotoController")
+const { insertPhoto, deletePhoto, getAllPhotos } = require("../controllers/PhotoController")
 
 // meddleware
 const { photoInsertValidation } = require("../middlewares/photoValidation")
@@ -13,5 +13,6 @@ const { imageUpload } = require("../middlewares/imageUpload")
 // routes
 router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto)
 router.delete("/:id", authGuard, deletePhoto)
+router.get("/", authGuard, getAllPhotos)
 
 module.exports = router
