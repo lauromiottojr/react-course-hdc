c8267bf19cdbae062cd211d20dda5b145c853116
lauromiottojr <junior.miotto36@gmail.com>
2023-01-25 11:11:53 -0300

    Removing photos

--- 11-reactgram/backend/controllers/PhotoController.js
+++ 11-reactgram/backend/controllers/PhotoController.js
@@ -22,12 +22,39 @@ const insertPhoto = async (req, res) => {
     // if photo was created, return data
     if (!newPhoto) {
         res.status(422).json({ errors: ["Houve um problema, por favor tente novamente mais tarde."] })
+        return
     }
 
     res.status(201).json(newPhoto)
 }
 
+// delete photo from db
+const deletePhoto = async (req, res) => {
+    const { id } = req.params
+    const reqUser = req.user
+
+    try {
+        const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+        // check if photo exists
+        if (!photo) {
+            res.status(404).json({ errors: ["Foto não encontrada."] })
+            return
+        }
+        // check if photo belongs to user
+        if (!photo.userId.equals(reqUser._id)) {
+            res.status(422).json({ errors: ["Ocorreu um erro, por favor tente novamente mais tarde."] })
+            return
+        }
+        await Photo.findByIdAndDelete(photo._id)
+        res.status(200).json({ id: photo._id, message: "Foto excluida com sucesso!" })
+    } catch (error) {
+        res.status(404).json({ errors: ["Foto não encontrada!"] })
+        return
+    }
+}
+
 module.exports = {
     insertPhoto,
+    deletePhoto,
 
 }
