7477fd64b5c3c60e2711bec1b1cedfeb85189ffb
lauromiottojr <junior.miotto36@gmail.com>
2023-01-24 21:36:13 -0300

    Getting user by id

--- 11-reactgram/backend/controllers/UserController.js
+++ 11-reactgram/backend/controllers/UserController.js
@@ -102,9 +102,27 @@ const update = async (req, res) => {
     res.status(200).json(user)
 }
 
+// get user by id
+const getUserById = async (req, res) => {
+    const { id } = req.params
+    try {
+        const user = await User.findById(mongoose.Types.ObjectId(id)).select("-password")
+        // check if user exists
+        if (!user) {
+            res.status(404).json({ errors: ["Usuário não encontrado!"] })
+            return
+        }
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(404).json({ errors: ["Usuário não encontrado!"] })
+        return
+    }
+}
+
 module.exports = {
     register,
     login,
     getCurrentUser,
     update,
+    getUserById,
 }
