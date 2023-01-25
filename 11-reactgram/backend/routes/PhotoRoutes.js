735ade506daebb22b0b8bf5a53391d16f5b184ef
lauromiottojr <junior.miotto36@gmail.com>
2023-01-25 10:18:28 -0300

    inserting new photo

--- 11-reactgram/backend/routes/PhotoRoutes.js
+++ 11-reactgram/backend/routes/PhotoRoutes.js
@@ -2,12 +2,15 @@ const express = require("express")
 const router = express.Router()
 
 // controller
+const { insertPhoto } = require("../controllers/PhotoController")
 
 // meddleware
 const { photoInsertValidation } = require("../middlewares/photoValidation")
 const authGuard = require("../middlewares/authGuard")
 const validate = require("../middlewares/handleValidation")
+const { imageUpload } = require("../middlewares/imageUpload")
 
 // routes
+router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto)
 
 module.exports = router
