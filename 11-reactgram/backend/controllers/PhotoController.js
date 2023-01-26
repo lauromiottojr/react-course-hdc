f2d3aa630b629576afde05533c6aa9bec27f4bde
lauromiottojr <junior.miotto36@gmail.com>
2023-01-25 21:11:28 -0300

    Like function

--- 11-reactgram/backend/controllers/PhotoController.js
+++ 11-reactgram/backend/controllers/PhotoController.js
@@ -104,6 +104,27 @@ const updatePhoto = async (req, res) => {
     res.status(200).json({ photo, message: "Foto atualizada com sucesso!" })
 }
 
+// like
+const likePhoto = async (req, res) => {
+    const { id } = req.params
+    const reqUser = req.user
+    const photo = await Photo.findById(id)
+    // check if photo exists
+    if (!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada."] })
+        return
+    }
+    // check if the user already liked the photo
+    if (photo.likes.includes(reqUser._id)) {
+        res.status(422).json({ errors: ["Você já curtiu a foto."] })
+        return
+    }
+    // put user id in likes array
+    photo.likes.push(reqUser._id)
+    photo.save()
+    res.status(200).json({ photoId: id, userId: reqUser._id, message: "Foto curtida com sucesso!" })
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -111,4 +132,5 @@ module.exports = {
     getUserPhotos,
     getPhotoById,
     updatePhoto,
+    likePhoto,
 }
