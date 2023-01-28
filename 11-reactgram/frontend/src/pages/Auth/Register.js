24a19721188b1fd5dd7b82d6a9eba5685f61be19
lauromiottojr <junior.miotto36@gmail.com>
2023-01-28 10:58:22 -0300

    register form

--- 11-reactgram/frontend/src/pages/Auth/Register.js
+++ 11-reactgram/frontend/src/pages/Auth/Register.js
@@ -1,8 +1,27 @@
+import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 import './Auth.css'
 
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>Reactgram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos de seus amigos</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Nome' />
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Senha' />
+        <input type="password" placeholder='Confirme a senha' />
+        <input type="submit" value='Cadastrar' />
+      </form>
+      <p>Já possui conta? <Link to='/login'>Faça seu login</Link></p>
+    </div>
   )
 }
 
