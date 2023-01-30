b039920c8bae4c907ec16798925748f6ab0c529a
lauromiottojr <junior.miotto36@gmail.com>
2023-01-30 14:56:09 -0300

    user authentication

--- 11-reactgram/frontend/src/App.js
+++ 11-reactgram/frontend/src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
+import { useAuth } from '../src/hooks/useAuth'
+
 import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
@@ -9,15 +11,22 @@ import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
+
+  const { auth, loading } = useAuth()
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className='container'>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
+            <Route path='/' element={auth ? <Home /> : <Navigate to='/login' />} />
+            <Route path='/login' element={!auth ? <Login /> : <Navigate to='/' />} />
+            <Route path='/register' element={!auth ? <Register /> : <Navigate to='/' />} />
           </Routes>
         </div>
         <Footer />
