a6ac80a653e7c10a58a6b66b7f90dacd74005687
lauromiottojr <junior.miotto36@gmail.com>
2023-01-31 10:21:46 -0300

    config login form

--- 11-reactgram/frontend/src/pages/Auth/Login.js
+++ 11-reactgram/frontend/src/pages/Auth/Login.js
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import Message from '../../components/Message/Message'
+
 import './Auth.css'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div id='login'>
-      
+      <h2>Reactgram</h2>
+      <p className='subtitle'>Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Email'
+          onChange={(e) => setEmail(e.target.value)} value={email || ""} />
+        <input type="password" placeholder='Senha'
+          onChange={(e) => setPassword(e.target.value)} value={password || ""} />
+        <input type="submit" placeholder='Entrar' />
+      </form>
+      <p>Não tem uma conta? <Link to='/register'>Clique aqui!</Link></p>
     </div>
   )
 }
