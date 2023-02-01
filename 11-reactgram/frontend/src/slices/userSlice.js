99d5453951c0b024002c1f4cb2d603f7dceb8b6c
lauromiottojr <junior.miotto36@gmail.com>
2023-01-31 21:07:22 -0300

    config userSlice

--- 11-reactgram/frontend/src/slices/userSlice.js
+++ 11-reactgram/frontend/src/slices/userSlice.js
@@ -1,5 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
+import userService from '../services/userService'
+
 const initialState = {
     user: {},
     error: false,
@@ -8,7 +10,13 @@ const initialState = {
     message: null,
 }
 
-// functions
+// get user details
+export const profile = createAsyncThunk("user/profile", async (user, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token
+    const data = await userService.profile(user, token)
+    return data
+}
+)
 
 export const userSlice = createSlice({
     name: "user",
@@ -17,8 +25,19 @@ export const userSlice = createSlice({
         resetMessage: (state) => {
             state.message = null
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(profile.pending, (state) => { // profile
+            state.loading = true
+            state.error = false
+        }).addCase(profile.fulfilled, (state, action) => {
+            state.loading = false
+            state.success = true
+            state.error = null
+            state.user = action.payload
+        })
     }
 })
 
-export const {resetMessage} = userSlice.actions
+export const { resetMessage } = userSlice.actions
 export default userSlice.reducer
