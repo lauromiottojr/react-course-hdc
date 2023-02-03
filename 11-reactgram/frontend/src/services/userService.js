b83a5af50721a880002bb99048b126e31ce29450
lauromiottojr <junior.miotto36@gmail.com>
2023-02-03 08:11:38 -0300

    config userService get user by id

--- 11-reactgram/frontend/src/services/userService.js
+++ 11-reactgram/frontend/src/services/userService.js
@@ -20,16 +20,31 @@ const updateProfile = async (data, token) => {
     try {
         const res = await fetch(api + "/users", config)
             .then((res) => res.json()).catch((err) => err)
-            return res
+        return res
     } catch (error) {
         console.log(error);
     }
 
 }
 
+// get user by id
+const getUserDetails = async (id) => {
+    const config = requestConfig("GET")
+
+    try {
+        const res = await fetch(api + "/users/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err)
+        return res
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 const userService = {
     profile,
     updateProfile,
+    getUserDetails,
 }
 
 export default userService
