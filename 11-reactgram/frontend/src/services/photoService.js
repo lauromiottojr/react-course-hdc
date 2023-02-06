7ac5affec5e7e521596a69ca7b239351b1086000
lauromiottojr <junior.miotto36@gmail.com>
2023-02-06 08:33:38 -0300

    config updatePhoto service

--- 11-reactgram/frontend/src/services/photoService.js
+++ 11-reactgram/frontend/src/services/photoService.js
@@ -26,12 +26,26 @@ const getUserPhotos = async (id, token) => {
     }
 }
 
-// delete a photo
+// delete photo
 const deletePhoto = async (id, token) => {
     const config = requestConfig("DELETE", null, token)
 
     try {
-        const res = fetch(api + "/photos/" + id, config)
+        const res = await fetch(api + "/photos/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err)
+        return res
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// update photo
+const updatePhoto = async (data, id, token) => {
+    const config = requestConfig("PUT", data, token)
+
+    try {
+        const res = await fetch(api + "/photos/" + id, config)
             .then((res) => res.json())
             .catch((err) => err)
         return res
