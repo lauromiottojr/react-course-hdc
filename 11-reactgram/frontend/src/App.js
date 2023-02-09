8a9982682a7b80ebe23fdc9f070958caaa1e5847
lauromiottojr <junior.miotto36@gmail.com>
2023-02-09 08:23:56 -0300

    Creating Photo page

--- 11-reactgram/frontend/src/App.js
+++ 11-reactgram/frontend/src/App.js
@@ -9,6 +9,7 @@ import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import EditProfile from './pages/EditProfile/EditProfile'
 import Profile from './pages/Profile/Profile'
+import Photo from './pages/Photo/Photo'
 
 import './App.css'
 
@@ -31,6 +32,7 @@ function App() {
             <Route path='/users/:id' element={auth ? <Profile /> : <Navigate to='/login' />} />
             <Route path='/login' element={!auth ? <Login /> : <Navigate to='/' />} />
             <Route path='/register' element={!auth ? <Register /> : <Navigate to='/' />} />
+            <Route path='/photos/:id' element={auth ? <Photo /> : <Navigate to='/login' />} />
           </Routes>
         </div>
         <Footer />
---
+++ 11-reactgram/frontend/src/pages/Photo/Photo.js
@@ -0,0 +1,17 @@
+import { upload } from '../../utils/config'
+
+import { Link, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import Message from '../../components/Message/Message'
+
+import './Photo.css'
+
+const Photo = () => {
+    return (
+        <div>Photo</div>
+    )
+}
+
+export default Photo
