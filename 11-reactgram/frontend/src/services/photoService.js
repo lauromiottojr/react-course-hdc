9483a7541726633a2c4d3433399e1488b02206a3
lauromiottojr <junior.miotto36@gmail.com>
2023-02-14 20:39:07 -0300

    config search service

--- 11-reactgram/frontend/src/services/photoService.js
+++ 11-reactgram/frontend/src/services/photoService.js
@@ -107,6 +107,19 @@ const getPhotos = async (token) => {
     }
 }
 
+// search photo by title
+const searchPhotos = async (query, token) => {
+    const config = requestConfig("GET", null, token)
+    try {
+        const res = await fetch(api + "/photos/search?query=", + query, config)
+            .then((res) => res.json())
+            .catch((err) => err)
+        return res
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export const photoService = {
     publishPhoto,
     getUserPhotos,
@@ -116,6 +129,7 @@ export const photoService = {
     like,
     comment,
     getPhotos,
+    searchPhotos,
 }
 
 export default photoService
