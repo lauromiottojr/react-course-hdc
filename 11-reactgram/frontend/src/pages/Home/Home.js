5519ea01cf659ed747125b6043f42b9f05df623d
lauromiottojr <junior.miotto36@gmail.com>
2023-02-14 14:07:49 -0300

    config Home component

--- 11-reactgram/frontend/src/pages/Home/Home.js
+++ 11-reactgram/frontend/src/pages/Home/Home.js
@@ -13,7 +13,7 @@ import './Home.css'
 const Home = () => {
 
   const dispatch = useDispatch()
-  const resetMessage = useResetComponentMessage()
+  const resetMessage = useResetComponentMessage(dispatch)
 
   const { user } = useSelector((state) => state.auth)
   const { photos, loading } = useSelector((state) => state.photo)
@@ -33,7 +33,19 @@ const Home = () => {
   }
 
   return (
-    <div>Home</div>
+    <div id='home'>
+      {photos && photos.map((photo) => (
+        <div key={photo._id}>
+          <PhotoItem photo={photo} />
+          <Like photo={photo} user={user} handleLike={handleLike} />
+          <Link className='btn' to={`/photos/${photo._id}`}>Ver mais</Link>
+        </div>
+      ))}
+      {photos && photos.length === 0 && (
+        <h2 className='noPhotos'>Ainda não há fotos publicadas,
+          <Link to={`/users/${user._id}`}> clique aqui e seja o primeiro a publicar!</Link></h2>
+      )}
+    </div>
   )
 }
 
