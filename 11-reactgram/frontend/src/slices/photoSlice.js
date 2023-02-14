f29a36ab724dd0b8b63388e29f098eb87933768a
lauromiottojr <junior.miotto36@gmail.com>
2023-02-14 20:40:38 -0300

    config search slice

--- 11-reactgram/frontend/src/slices/photoSlice.js
+++ 11-reactgram/frontend/src/slices/photoSlice.js
@@ -98,6 +98,14 @@ export const getPhotos = createAsyncThunk("photo/getall", async (_, thunkAPI) =>
 }
 )
 
+// search photo by title
+export const searchPhotos = createAsyncThunk("photo/search", async (query, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token
+    const data = await photoService.searchPhotos(query, token)
+    return data
+}
+)
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -202,6 +210,14 @@ export const photoSlice = createSlice({
             state.loading = true
             state.error = null
         }).addCase(getPhotos.fulfilled, (state, action) => {
+            state.loading = false
+            state.success = true
+            state.error = null
+            state.photos = action.payload
+        }).addCase(searchPhotos.pending, (state) => { // search photos
+            state.loading = true
+            state.error = null
+        }).addCase(searchPhotos.fulfilled, (state, action) => {
             state.loading = false
             state.success = true
             state.error = null
