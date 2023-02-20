45dd049acbf12fdd493e1ecde5ccda40d16fdd18
lauromiottojr <junior.miotto36@gmail.com>
2023-02-20 11:17:33 -0300

    create footer and header

---
+++ 13-todo-react-ts/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer>
+            <p><span>React + TS To Do</span> @ 2023</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ 13-todo-react-ts/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>React + TS To Do</h1>
+        </header>
+    )
+}
+
+export default Header
