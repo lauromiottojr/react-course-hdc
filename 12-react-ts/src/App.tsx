4a4ddcad5a048da0b77bdda6fa18d00c1a63bcd3
lauromiottojr <junior.miotto36@gmail.com>
2023-02-20 08:50:02 -0300

    ts functions

--- 12-react-ts/src/App.tsx
+++ 12-react-ts/src/App.tsx
@@ -4,12 +4,17 @@ function App() {
   const age: number = 30
   const isWorking: Boolean = true
 
+  const userGreeting = (name: String): String => {
+    return `Olá, ${name}!`
+  }
+
   return (
     <div className="App">
       <h1>TypeScript + React</h1>
       <p>Nome: {name}</p>
       <p>Idade: {age}</p>
       <p>{name}{isWorking ? (<span> está</span>) : (<span> não está</span>)} trabalhando!</p>
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
