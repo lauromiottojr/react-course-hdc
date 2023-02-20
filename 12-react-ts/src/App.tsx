5fcf7ecbad15a49832082d5056243f15f94d005f
lauromiottojr <junior.miotto36@gmail.com>
2023-02-20 10:16:27 -0300

    ts context

--- 12-react-ts/src/App.tsx
+++ 12-react-ts/src/App.tsx
@@ -1,9 +1,19 @@
+import { createContext } from 'react'
+
 import Destructuring, { Category } from "./components/Destructuring"
 import FirstComponent from "./components/FirstComponent"
 import SecondComponent from "./components/SecondComponent"
 import State from "./components/State"
+import Context from './components/Context'
 
 type textOrNull = string | null
+
+interface IAppContext {
+  language: String,
+  framework: String,
+}
+
+export const AppContext = createContext<IAppContext | null>(null)
 
 function App() {
 
@@ -18,19 +28,28 @@ function App() {
   const myText: textOrNull = "Tem texto aqui!"
   let myOtherText: textOrNull = null
 
+  const contextValue: IAppContext = {
+    language: "JS",
+    framework: "react",
+  }
+
+
   return (
-    <div className="App">
-      <h1>TypeScript + React</h1>
-      <p>Nome: {name}</p>
-      <p>Idade: {age}</p>
-      <p>{name}{isWorking ? (<span> está</span>) : (<span> não está</span>)} trabalhando!</p>
-      <h3>{userGreeting(name)}</h3>
-      <FirstComponent />
-      <SecondComponent name={name} />
-      <Destructuring title="Meu post" commentQtd={50} content="Algum conteúdo!"
-        tags={["vapo", "cepo de madeira"]} category={Category.J} />
-      <State />
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>TypeScript + React</h1>
+        <p>Nome: {name}</p>
+        <p>Idade: {age}</p>
+        <p>{name}{isWorking ? (<span> está</span>) : (<span> não está</span>)} trabalhando!</p>
+        <h3>{userGreeting(name)}</h3>
+        <FirstComponent />
+        <SecondComponent name={name} />
+        <Destructuring title="Meu post" commentQtd={50} content="Algum conteúdo!"
+          tags={["vapo", "cepo de madeira"]} category={Category.J} />
+        <State />
+        <Context />
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ 12-react-ts/src/components/Context.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react'
+
+import { AppContext } from '../App'
+
+const Context = () => {
+
+    const details = useContext(AppContext)
+
+    return (
+        <>
+            {details && (
+                <div>
+                    <h2>Linguagem: {details.language}</h2>
+                    <h2>Framework: {details.framework}</h2>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default Context
